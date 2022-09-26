9a2379a81e2c0b0b2f9afdfce9239ab0718b2e3c
aryansyedECE42 <94012277+aryansyedECE42@users.noreply.github.com>
2022-09-26 20:57:00 +0530

    Add files via upload

---
+++ SOCKS1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;cin>>a>>b>>c;
+	if(a==b||b==c||a==c||a==b==c)
+	cout<<"yes"<<endl;
+	else
+	cout<<"no"<<endl;
+	return 0;
+}
